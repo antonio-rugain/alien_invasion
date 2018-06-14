ea8dd60973a895ab3b32bfbc08838a25a6699bae
antonio-rugain <antonio.rugain@gmail.com>
2018-06-14 17:42:21 -0300

    Refatoracao de diretorios

---
+++ game/game/settings.py
@@ -0,0 +1,6 @@
+class Settings():   #classe que guarda as configuraçoes do jogo
+
+    def __init__(self):
+        self.screen_width = 1300
+        self.screen_height =700
+        self.bg_color = (230, 230, 230)
